602d839966bd235f0171583cbe9c373a6d260bca
Michael Keith <mikekeith52@gmail.com>
2022-05-15 12:37:14 -0600

    Worked on feature selection notebook

---
+++ misc/feature-selection/config.py
@@ -0,0 +1,24 @@
+model_params = {
+    'test_only' : True,
+    'dynamic_testing' : False,
+}
+
+dynamic_tuning = False
+patience = 7
+
+grids = {
+    'gbt':dict(
+        n_estimators=[10000],
+        max_depth=[2],
+        max_features=['log2'],
+        random_state=[20],
+    ),
+    'lasso':dict(
+        alpha=[0.025],
+    ),
+    'xgboost':dict(
+        n_estimators=[2000,5000,10000],
+        subsample=[0.8,0.9],
+        max_depth=[1,2,3,6], 
+    ),
+}
