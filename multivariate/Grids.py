0d369309f4ed7e6c8ae4f4133a05617e66000349
Michael Keith <mikekeith52@gmail.com>
2026-03-01 17:33:18 -0700

    Updating multivariate notebook

--- multivariate/Grids.py
+++ multivariate/Grids.py
@@ -1,7 +1,6 @@
 arima = {
     'order':[(2,1,0),(0,1,2),(1,1,1)],
     'seasonal_order':[(0,1,1,12),(2,1,0,12)],
-    'trend':['n','c','t','ct'],
 }
 
 catboost = {
@@ -9,6 +8,7 @@ catboost = {
     'learning_rate': [0.01, 0.05, 0.1],
     'depth': [4, 6, 8],
     'l2_leaf_reg': [1, 3, 5, 7, 9],
+    'verbose': [0],
 }
 
 elasticnet = {
@@ -18,7 +18,7 @@ elasticnet = {
 }
 
 gbt = {
-    'max_depth':[2,3],
+    'max_depth':[2,3,4,5],
     'max_features':['sqrt',None],
 }
 
@@ -29,7 +29,11 @@ hwes = {
 }
 
 knn = {
-    'n_neighbors':range(2,21),
+    'n_neighbors':range(2,101),
+}
+
+lasso = {
+    'alpha':[i/100 for i in range(1,101)],
 }
 
 lightgbm = {
@@ -37,10 +41,16 @@ lightgbm = {
     'boosting_type':['gbdt','dart','goss'],
     'max_depth':[1,2,3],
     'learning_rate':[0.001,0.01,0.1],
+    'verbosity':[-1],
 }
 
-lasso = {
-    'alpha':[i/100 for i in range(1,101)],
+lstm = {
+    'lstm_layer_sizes':[(50,50,50)],
+    'activation':['relu','tanh'],
+    'dropout':[(0,0,0),(.2,.2,.2),],
+    'lags':[10,25,50],
+    'verbose':[0],
+    'epochs':[25],
 }
 
 mlp = {
@@ -54,6 +64,10 @@ mlr = {
     'normalizer':['scale','minmax',None],
 }
 
+naive = {
+    'seasonal':[True,False],
+}
+
 prophet = {
     'n_changepoints':range(5),
 }
@@ -61,7 +75,7 @@ prophet = {
 rf = {
     'max_depth':[2,5],
     'n_estimators':[100,500],
-    'max_features':['auto','sqrt'],
+    'max_features':[.5,'sqrt'],
     'max_samples':[.75,.9,1],
 }
 
@@ -69,8 +83,31 @@ ridge = {
     'alpha':[i/100 for i in range(1,101)],
 }
 
+rnn = {
+    'layers_struct':[
+        [
+            ('LSTM',{'units':50,'activation':'relu'}),
+            ('LSTM',{'units':50,'activation':'relu'}),
+            ('LSTM',{'units':50,'activation':'relu'}),
+        ],
+        [
+            ('LSTM',{'units':50,'activation':'tanh'}),
+            ('LSTM',{'units':50,'activation':'tanh'}),
+            ('LSTM',{'units':50,'activation':'tanh'}),
+        ],
+        [
+            ('LSTM',{'units':50,'activation':'tanh','dropout':.2}),
+            ('LSTM',{'units':50,'activation':'tanh','dropout':.2}),
+            ('LSTM',{'units':50,'activation':'tanh','dropout':.2}),
+        ],
+    ],
+    'epochs':[25],
+    'verbose':[0],
+}
+
 silverkite = {
-    'changepoints':range(5),
+    'changepoints':[2,None],
+    'cv_max_splits':[0,3],
 }
 
 sgd={
@@ -85,8 +122,20 @@ svr={
     'epsilon':[0.01,0.1,0.5],
 }
 
+tbats = {
+    'seasonal_periods':[[12],None],
+}
+
 theta = {
     'theta':[0.5,1,1.5,2],
+}
+
+vecm = {
+    'lags':[0],
+    'normalizer':[None],
+    'k_ar_diff':[1,2,3,4,5,6,7],
+    'deterministic':["n","co","lo","li","cili","colo"],
+    'seasons':[0,12],
 }
 
 xgboost = {
